80d281cf4b7639aa4fc276f048ce1611c752d807
sarvaiya-sunil <sarvaiyasunil25@gmail.com>
2026-01-05 14:48:44 +0530

    untrack one file

--- frontend/notes-app/src/pages/Home/Login/Login.jsx
+++ frontend/notes-app/src/pages/Home/Login/Login.jsx
@@ -50,7 +50,6 @@ const Login = () => {
         <div className="w-96 border rounded bg-white px-7 py-10">
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl mb-7">Login</h4>
-
             <input
               type="text"
               placeholder="email"
@@ -59,7 +58,6 @@ const Login = () => {
               value={email}
               onChange={(e) => setEmail(e.target.value)}
             />
-
             <PasswordInput
               value={password}
               onChange={(e) => setPassword(e.target.value)}
